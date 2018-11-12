2c13f7289bea77092054d356d3d5935fa366719e
Stanislav <its.an.omen.man@gmail.com>
2018-11-12 22:53:02 +0200

    Mock test for MeasurementService

--- server/src/test/java/edu/iss/gitcloneteam/weatherobserver/WeatherobserverApplicationTests.java
+++ server/src/test/java/edu/iss/gitcloneteam/weatherobserver/WeatherobserverApplicationTests.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
 
 import java.io.*;
 
