4ea249a24a58d5422715e8eabe3f39e8bdfc677e
Stanislav <its.an.omen.man@gmail.com>
2018-11-12 12:11:53 +0200

    Model configuration for measurement with mapper

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS weather_observer.measurement;
 CREATE SCHEMA IF NOT EXISTS weather_observer;
 
 CREATE TABLE IF NOT EXISTS weather_observer.measurement (
-   id INT NOT NULL,
+   measurement_id INT NOT NULL,
    temperature INT NOT NULL,
    pressure INT NOT NULL,
-   submission_date TIMESTAMP NOT NULL,
-   PRIMARY KEY(id)
+   measurement_time TIMESTAMP NOT NULL,
+   PRIMARY KEY(measurement_id)
 );
 
