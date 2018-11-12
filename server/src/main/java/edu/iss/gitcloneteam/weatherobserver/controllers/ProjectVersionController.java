718735580c00d66e6d15962a1ab68905d584617f
Stanislav <its.an.omen.man@gmail.com>
2018-11-12 20:45:54 +0200

    describe git version and return it later by request

--- server/src/main/java/edu/iss/gitcloneteam/weatherobserver/controllers/ProjectVersionController.java
+++ server/src/main/java/edu/iss/gitcloneteam/weatherobserver/controllers/ProjectVersionController.java
@@ -1,5 +1,7 @@
 package edu.iss.gitcloneteam.weatherobserver.controllers;
 
+import edu.iss.gitcloneteam.weatherobserver.utils.ProjectVersionReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/version")
 public class ProjectVersionController {
 
+    private ProjectVersionReader projectVersionReader;
+
+    @Autowired
+    public ProjectVersionController(ProjectVersionReader projectVersionReader) {
+        this.projectVersionReader = projectVersionReader;
+    }
+
     @GetMapping("/current")
     public String getCurrentProjectVersion(){
-        // stub
-        return "1.0.0";
+        String currentVersion = projectVersionReader.readVersion();
+        if (currentVersion == null) {
+            currentVersion = ProjectVersionReader.DEFAULT_VERSION;
+        }
+        return currentVersion;
     }
 
 }
