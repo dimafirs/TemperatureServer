eac4289bda0a9a05d777b03e5eef21b942c18bab
vise0418 <vitalii.semchyshyn@netcracker.com>
2018-12-24 22:41:36 +0200

    Add little design

--- front/src/app/model/duration.ts
+++ front/src/app/model/duration.ts
@@ -8,6 +8,6 @@ export const DURATIONS: Duration[] =
     {value: 3, name: '3 hours'},
     {value: 6, name: '6 hours'},
     {value: 12, name: '12 hours'},
-    {value: 24, name: 'day'},
-    {value: 168, name: 'week'}
+    {value: 24, name: 'Day'},
+    {value: 168, name: 'Week'}
   ];
